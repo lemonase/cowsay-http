7106995a7dfbbd4bc62733a043e3e86f70d52f33
James Dixon <notjamesdixon@gmail.com>
2024-06-23 23:38:41 -0400

    remove build script as we are using GH actions

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# Build and push docker image for amd64 and arm64
-
-docker buildx build --platform linux/amd64 -t jamesdixon/cowsay-http:latest-amd64 . && docker push jamesdixon/cowsay-http:latest-amd64
-docker buildx build --platform linux/arm64 -t jamesdixon/cowsay-http:latest-arm64 . && docker push jamesdixon/cowsay-http:latest-arm64
